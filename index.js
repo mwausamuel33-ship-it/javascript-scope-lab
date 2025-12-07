4ef9ff213ac722605653a4f2846447df6ebd8b1c
samuel <mwausamuel33@gmail.com>
2025-12-07 20:31:48 +0300

    flatburger lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+const burgers = ["Humberger", "CheeseBurger"];
+let featuredDrink = "strawberry Milkshake";
+
+function addburger() {
+  const newburger = "flatburger";
+  burgers.push(newburger);
+}
+
+if (true) {
+  const anotherNewBurger = "Maple Bacon Burger";
+  burgers.push(anotherNewBurger);
+}
+
+function changeFeaturedDrink() {
+  featuredDrink = "The JavaShake";
+}
